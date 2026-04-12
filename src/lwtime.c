9980891690680b6dc1950414bb3668d238b6a179
pandora-wolf-meow <23092261+pandora-wolf-meow@users.noreply.github.com>
2026-04-12 04:30:15 -0700

    Revert "[perf] halve game time + early termination for 2-3x faster training"
    This reverts commit 9f3f24d10cd09d29fac86d1d2d6cb19659c95692.

--- src/lwtime.c
+++ src/lwtime.c
@@ -96,9 +96,9 @@ update_play_time (void)
     {
       /*
        * In headless mode, simulate time from GLOBAL_CLOCK.
-       * Use 200 ticks/sec for faster games during training.
+       * Assume ~100 logic ticks per second.
        */
-      TIME_ELAPSED = GLOBAL_CLOCK / 50;
+      TIME_ELAPSED = GLOBAL_CLOCK / 100;
       TIME_LEFT = TIME_TABLE[LW_CONFIG_CURRENT_RULES.game_time] - TIME_ELAPSED;
       if (TIME_LEFT < 0)
         TIME_LEFT = 0;
